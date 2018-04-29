1b27dcd9c6b6feb499517d1877df3c630d277414
Weihang Lo <weihanglotw@gmail.com>
2018-04-29 18:19:51 +0800

    add stability test cases

--- src/sorting/test_cases.rs
+++ src/sorting/test_cases.rs
@@ -1,3 +1,4 @@
+/// Common test cases for any sorting algorithms accepting i32 array as input.
 macro_rules! base_cases { 
     ($algo:ident) => {
         fn assert(arr: &mut [i32], res: &[i32]) {
@@ -16,6 +17,60 @@ macro_rules! base_cases {
         fn one_element() {
             let mut arr = [1];
             let res = [1];
+            assert(&mut arr, &res);
+        }
+
+        #[test]
+        fn two_elements() {
+            let mut arr = [1, 2];
+            let res = [1, 2];
+            assert(&mut arr, &res);
+
+            let mut arr = [2, 1];
+            let res = [1, 2];
+            assert(&mut arr, &res);
+
+            let mut arr = [1, 1];
+            let res = [1, 1];
+            assert(&mut arr, &res);
+        }
+
+        #[test]
+        fn three_elements() {
+            let mut arr = [1, 1, 2];
+            let res = [1, 1, 2];
+            assert(&mut arr, &res);
+
+            let mut arr = [2, 1, 1];
+            let res = [1, 1, 2];
+            assert(&mut arr, &res);
+
+            let mut arr = [1, 2, 1];
+            let res = [1, 1, 2];
+            assert(&mut arr, &res);
+
+            let mut arr = [1, 2, 3];
+            let res = [1, 2, 3];
+            assert(&mut arr, &res);
+
+            let mut arr = [1, 3, 2];
+            let res = [1, 2, 3];
+            assert(&mut arr, &res);
+
+            let mut arr = [2, 1, 3];
+            let res = [1, 2, 3];
+            assert(&mut arr, &res);
+
+            let mut arr = [2, 3, 1];
+            let res = [1, 2, 3];
+            assert(&mut arr, &res);
+
+            let mut arr = [3, 1, 2];
+            let res = [1, 2, 3];
+            assert(&mut arr, &res);
+
+            let mut arr = [3, 2, 1];
+            let res = [1, 2, 3];
             assert(&mut arr, &res);
         }
 
@@ -42,8 +97,12 @@ macro_rules! base_cases {
 
         #[test]
         fn duplicated() {
-            let mut arr = [1, 5, 3, 3, 4, 1, 4];
-            let res = [1, 1, 3, 3, 4, 4, 5];
+            let mut arr = [1, 5, 3, 3, 4, 1, 3, 4];
+            let res = [1, 1, 3, 3, 3, 4, 4, 5];
+            assert(&mut arr, &res);
+
+            let mut arr = [3, 1, 3, 3, 3, 2, 2, 1, 2, 1, 2, 3, 3, 2, 1];
+            let res = [1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3];
             assert(&mut arr, &res);
         }
 
@@ -59,6 +118,52 @@ macro_rules! base_cases {
             let mut arr = [7, 1, 2, 4, 6, 5, 3];
             let res = [1, 2, 3, 4, 5, 6, 7];
             assert(&mut arr, &res);
+        }
+    }
+}
+
+/// Test cases for validate stability of sorting algorithm.
+/// The input value is a tuple type of (i32, i32).
+///
+/// To test with these cases, an algorithm must accept generic input values.
+macro_rules! stability_cases {
+    ($algo:ident) => {
+        /// (key, value)
+        fn assert_stability(arr: &mut [(i32, i32)], res: &[(i32, i32)]) {
+            $algo(arr);
+            assert_eq!(arr, res);
+        }
+
+        #[test]
+        fn random() {
+            let mut arr = [
+                (2, 1), (1, 1), (3, 1), (2, 2), (2, 3),
+                (3, 2), (1, 2), (1, 3), (3, 3), (3, 4),
+                (1, 4), (1, 5), (3, 5), (2, 4), (2, 5),
+            ];
+            let res = [
+                (1, 1), (1, 2), (1, 3), (1, 4), (1, 5),
+                (2, 1), (2, 2), (2, 3), (2, 4), (2, 5),
+                (3, 1), (3, 2), (3, 3), (3, 4), (3, 5),
+            ];
+            assert_stability(&mut arr, &res);
+        }
+
+        #[test]
+        fn interleave() {
+            let mut arr = [
+                (1, 1), (2, 1), (3, 1),
+                (1, 2), (2, 2), (3, 2),
+                (1, 3), (2, 3), (3, 3),
+                (1, 4), (2, 4), (3, 4),
+                (1, 5), (2, 5), (3, 5),
+            ];
+            let res = [
+                (1, 1), (1, 2), (1, 3), (1, 4), (1, 5),
+                (2, 1), (2, 2), (2, 3), (2, 4), (2, 5),
+                (3, 1), (3, 2), (3, 3), (3, 4), (3, 5),
+            ];
+            assert_stability(&mut arr, &res);
         }
     }
 }
