b6d5d4caaf9e34fe82d8977b5003f0c2159682c0
Weihang Lo <weihanglotw@gmail.com>
2018-05-01 16:22:38 +0800

    add manual publish script [ci skip]

---
+++ publish.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+TARGET_BRANCH="gh-pages"
+PUBLIC_DIR=".book"
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf "${PUBLIC_DIR}"
+mkdir "${PUBLIC_DIR}"
+git worktree prune
+rm -rf ".git/worktrees/${PUBLIC_DIR}/"
+
+echo "Checking out branch ${TARGET_BRANCH} into ${PUBLIC_DIR}"
+git worktree add -B "${TARGET_BRANCH}" "${PUBLIC_DIR}" "origin/${TARGET_BRANCH}"
+
+echo "Removing existing files"
+rm -rf "${PUBLIC_DIR}/*"
+
+echo "Generating site"
+mdbook build -d _tmp_book
+cp -rp _tmp_book/* "${PUBLIC_DIR}/"
+rm -rf _tmp_book
+
+echo "Updating branch ${TARGET_BRANCH}"
+cd "${PUBLIC_DIR}" && git add --all && \
+    git commit -m "Publishing via publish.sh at $(date)" && \
+    git push
