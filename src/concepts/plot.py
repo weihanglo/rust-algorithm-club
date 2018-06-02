3043446055b0fa8c5d10096ea71a928ac8cd71d5
Weihang Lo <weihanglotw@gmail.com>
2018-06-02 11:45:09 +0800

    update plots

---
+++ src/concepts/plot.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+OUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fig')
+os.makedirs(OUT_DIR)
+
+def plot_asymptotic_notation():
+  """Generate figures for 'Asymptotic Notation' chapter."""
+  plt.rc('lines', linewidth=3)
+  plt.rc('font', size=14)
+
+  # Fig. 1
+  x = np.linspace(1, 10)
+  plt.plot(x, np.log(x), label='Logarithmic O(log(n))', linewidth=3)
+  plt.plot(x, x, label='Linear O(n)', linewidth=3)
+  plt.plot(x, x * np.log(x), label='Linearithmic O(n log(n))', linewidth=3)
+  plt.xlabel('Size of Input')
+  plt.ylabel('Cost')
+  plt.legend()
+  plt.savefig(os.path.join(OUT_DIR, 'asymptotic-notation-1.png'))
+  plt.clf()
+
+  # Fig. 2
+  x = np.linspace(1, 30)
+  plt.plot(x, 10 * x + 29, label='f(x) = 10x + 29')
+  plt.plot(x, x**2 + 1, label='g(x) = x^2 + 1')
+  plt.xlabel('Size of Input')
+  plt.ylabel('Cost')
+  plt.legend()
+  plt.savefig(os.path.join(OUT_DIR, 'asymptotic-notation-2.png'))
+  plt.clf()
+
+  # Fig. 3
+  x = np.linspace(1, 10)
+  plt.axvline(x=4, linestyle='dashed', color='lightgray', linewidth=2)
+  plt.plot(x, 3 * x + 4, label='f(n) = 3n + 4')
+  plt.plot(x, 4 * x, label='g(n) = 4n')
+  plt.xlabel('Size of Input')
+  plt.ylabel('Cost')
+  plt.legend()
+  plt.savefig(os.path.join(OUT_DIR, 'asymptotic-notation-3.png'))
+  plt.clf()
+
+  # Fig. 4
+  x = np.linspace(1, 10)
+  plt.axvline(x=2, linestyle='dashed', color='lightgray', linewidth=2)
+  plt.plot(x, 3 * x + 4, label='f(n) = 3n + 4')
+  plt.plot(x, x, label='k1 * g(n) = n')
+  plt.plot(x, 5 * x, label='k2 * g(n) = 5n')
+  plt.xlabel('Size of Input')
+  plt.ylabel('Cost')
+  plt.legend()
+  plt.savefig(os.path.join(OUT_DIR, 'asymptotic-notation-4.png'))
+  plt.clf()
+
+  # Close current window
+  plt.close()
+
+def main():
+    """Main"""
+    plot_asymptotic_notation()
+
+main()
