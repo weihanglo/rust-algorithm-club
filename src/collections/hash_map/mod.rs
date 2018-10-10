8cfb63ea85a9c7dad8345abb881311f7ffc11819
choznerol <lawrencechou1024@gmail.com>
2018-10-10 09:19:21 +0800

    Revert "Implement HashSet::union()"
    This reverts commit caab7fbc5323e09eb1e30ea374eb21a59f955bad.

--- src/collections/hash_map/mod.rs
+++ src/collections/hash_map/mod.rs
@@ -18,7 +18,6 @@ use std::mem;
 ///
 /// [1]: https://doc.rust-lang.org/stable/std/collections/struct.HashMap.html
 /// [2]: https://en.cppreference.com/w/cpp/container/unordered_map
-#[derive(Clone)]
 pub struct HashMap<K, V> where K: Hash + Eq {
     buckets: Vec<Bucket<K, V>>,
     len: usize,
