48ace8552119483d6099e02ebcb437a5ac4aa439
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-17 15:18:25 +0000

    Deploy weihanglo/rust-algorithm-club to github.com/weihanglo/rust-algorithm-club.git:gh-pages

--- searching/interpolation_search/mod.rs
+++ searching/interpolation_search/mod.rs
@@ -1,4 +1,79 @@
-/// Interpolation search.
-pub fn interpolation_search() {
-    unimplemented!();
+/// Search in sorted sequences by checking the next position based on an
+/// linear interpolation of the search key.
+///
+/// # Parameters
+///
+/// * `arr`: Slice to search in.
+/// * `target`: Object to search for.
+///
+/// # Notes
+///
+/// Since interpolations only be applied on numeric types, we choose `i32` to
+/// avoid overkilling. If you desire a full functional trait of numeric types,
+/// [num][1] crate would meet your needs.
+///
+/// [1]: https://github.com/rust-num/num
+pub fn interpolation_search(
+    arr: &[i32],
+    target: &i32,
+) -> Result<usize, usize> {
+    // 1. Handle empty sequence.
+    if arr.is_empty() {
+        return Err(0)
+    }
+
+    // 2. Setup variable storing iteration informaion.
+    // hi -> upper bound of search range.
+    // lo -> lower bound of search range.
+    // interpolant -> position to probe in the sequence
+    let mut hi = arr.len() - 1;
+    let mut lo = 0_usize;
+    let mut interpolant = 0_usize;
+
+    // 3. Main loop to calculate the interpolant.
+    loop {
+        let lo_val = arr[lo];
+        let hi_val = arr[hi];
+
+        // 3.1. Three condition to exit the loop
+        //   a. hi and lo flag overlapping -> all elements are scanned.
+        //   b. target value is less than the lowest value
+        //   c. target value exceeds the highest value
+        if hi <= lo || *target < lo_val || *target > hi_val {
+            break
+        }
+
+        // 3.2. The linear interpolation part
+        let offset = (*target - lo_val) * (hi - lo) as i32 / (hi_val - lo_val);
+        interpolant = lo + offset as usize;
+
+        let mid_val = arr[interpolant];
+
+        // 3.3. Comparison between the interpolant and targert value.
+        // New boundaries must step one index further to avoid infinite searching.
+        if mid_val > *target {
+            hi = interpolant - 1;
+        } else if mid_val < *target {
+            lo = interpolant + 1;
+        } else {
+            break
+        }
+    }
+
+    // 4. Determine whether the returning interpolant equals to target value.
+    // `Result::Err` here maps to a position safe to insert while remains ordering.
+    if *target > arr[hi] {
+        Err(hi + 1)
+    } else if *target < arr[lo] {
+        Err(lo)
+    } else {
+        Ok(interpolant)
+    }
+}
+
+#[cfg(test)]
+mod base {
+    use super::*;
+
+    sorted_no_duplicate_cases!(interpolation_search);
 }
--- searching/test_cases.rs
+++ searching/test_cases.rs
@@ -63,6 +63,35 @@ macro_rules! base_cases {
             let expected = None;
             assert_eq!($algo(arr, target), expected);
         }
+
+        #[test]
+        fn random() {
+            // Similar behavior as `std::slice::binary_search`.
+            //
+            // Test for
+            //
+            // - random array lengh
+            // - random elements
+            // - random target
+            use rand;
+
+            for _ in 0..100 {
+                let len = rand::random::<usize>() % 500;
+                let mut arr: Vec<_> = (0..len)
+                    .map(|_| rand::random::<i32>() % 2000)
+                    .collect();
+                arr.sort_unstable();
+                arr.dedup();
+
+                (0..50).for_each(|_| {
+                    let target = rand::random::<i32>() % 4000 - 1000;
+                    assert_eq!(
+                        arr.binary_search(&target).ok(),
+                        $algo(&arr, &target),
+                    )
+                })
+            }
+        }
     }
 }
 
@@ -83,12 +112,12 @@ macro_rules! sorted_no_duplicate_cases {
             let arr = &[0];
             let target = &0;
             let expected = Ok(0);
-            assert_eq!($algo(arr, target), expected);
+            assert_eq!($algo(arr, target), expected, "0 -> Ok(0)");
 
             let arr = &[0];
             let target = &1;
             let expected = Err(1);
-            assert_eq!($algo(arr, target), expected);
+            assert_eq!($algo(arr, target), expected, "1 -> Err(1)");
         }
 
         #[test]
@@ -110,27 +139,48 @@ macro_rules! sorted_no_duplicate_cases {
         #[test]
         fn multiple() {
             let arr = &[
-                0, 1, 2, 3, 4,
-                5, 6, 7, 8, 9,
+                0, 2, 4, 6, 8,
+                10, 12, 14, 16 ,18,
             ];
             let target = &0;
             let expected = Ok(0);
             assert_eq!($algo(arr, target), expected);
 
-            let target = &1;
+            let target = &2;
             let expected = Ok(1);
             assert_eq!($algo(arr, target), expected);
 
-            let target = &2;
+            let target = &4;
             let expected = Ok(2);
             assert_eq!($algo(arr, target), expected);
 
-            let target = &3;
+            let target = &6;
             let expected = Ok(3);
             assert_eq!($algo(arr, target), expected);
 
-            let target = &10;
+
+            let target = &1;
+            let expected = Err(1);
+            assert_eq!($algo(arr, target), expected);
+
+            let target = &3;
+            let expected = Err(2);
+            assert_eq!($algo(arr, target), expected);
+
+            let target = &5;
+            let expected = Err(3);
+            assert_eq!($algo(arr, target), expected);
+
+            let target = &7;
+            let expected = Err(4);
+            assert_eq!($algo(arr, target), expected);
+
+            let target = &20;
             let expected = Err(10);
+            assert_eq!($algo(arr, target), expected);
+
+            let target = &-1;
+            let expected = Err(0);
             assert_eq!($algo(arr, target), expected);
         }
 
@@ -140,6 +190,35 @@ macro_rules! sorted_no_duplicate_cases {
             let target = &888;
             let expected = Err(3);
             assert_eq!($algo(arr, target), expected);
+        }
+
+        #[test]
+        fn random() {
+            // Same behavior as `std::slice::binary_search`.
+            //
+            // Test for
+            //
+            // - random array lengh
+            // - random elements
+            // - random target
+            use rand;
+
+            for _ in 0..100 {
+                let len = rand::random::<usize>() % 500;
+                let mut arr: Vec<_> = (0..len)
+                    .map(|_| rand::random::<i32>() % 2000)
+                    .collect();
+                arr.sort_unstable();
+                arr.dedup();
+
+                (0..50).for_each(|_| {
+                    let target = rand::random::<i32>() % 4000 - 1000;
+                    assert_eq!(
+                        arr.binary_search(&target),
+                        $algo(&arr, &target),
+                    )
+                })
+            }
         }
     }
 }
