675eefe2fc4ad084628944dbb82464eba0f6b433
weihanglo <weihanglo@users.noreply.github.com>
2020-04-19 01:19:53 +0000

    Automated deployment: Sun Apr 19 01:19:53 UTC 2020 d93eba584a68bfdf2a23d682bea28e7c7d482863

--- doc/implementors/core/cmp/trait.PartialOrd.js
+++ doc/implementors/core/cmp/trait.PartialOrd.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
-implementors["rust_algorithm_club"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"rust_algorithm_club/collections/struct.HashSet.html\" title=\"struct rust_algorithm_club::collections::HashSet\">HashSet</a>&lt;T&gt;&gt; for <a class=\"struct\" href=\"rust_algorithm_club/collections/struct.HashSet.html\" title=\"struct rust_algorithm_club::collections::HashSet\">HashSet</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>,&nbsp;</span>",synthetic:false,types:["rust_algorithm_club::collections::set::HashSet"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+implementors["rust_algorithm_club"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"rust_algorithm_club/collections/struct.HashSet.html\" title=\"struct rust_algorithm_club::collections::HashSet\">HashSet</a>&lt;T&gt;&gt; for <a class=\"struct\" href=\"rust_algorithm_club/collections/struct.HashSet.html\" title=\"struct rust_algorithm_club::collections::HashSet\">HashSet</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>,&nbsp;</span>","synthetic":false,"types":["rust_algorithm_club::collections::set::HashSet"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/core/iter/traits/collect/trait.IntoIterator.js
+++ doc/implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
-implementors["rust_algorithm_club"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"rust_algorithm_club/collections/struct.SinglyLinkedList.html\" title=\"struct rust_algorithm_club::collections::SinglyLinkedList\">SinglyLinkedList</a>&lt;T&gt;",synthetic:false,types:["rust_algorithm_club::collections::singly_linked_list::SinglyLinkedList"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+implementors["rust_algorithm_club"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"rust_algorithm_club/collections/struct.SinglyLinkedList.html\" title=\"struct rust_algorithm_club::collections::SinglyLinkedList\">SinglyLinkedList</a>&lt;T&gt;","synthetic":false,"types":["rust_algorithm_club::collections::singly_linked_list::SinglyLinkedList"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
