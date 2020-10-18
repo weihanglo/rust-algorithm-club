f3d7a4e892bd97c52939403da9bdfd277b3305d4
weihanglo <weihanglo@users.noreply.github.com>
2020-10-18 16:15:58 +0000

    deploy: 0615a1a47c0573d70147ed4e3d728f93627641d8

--- doc/implementors/core/fmt/trait.Debug.js
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["rust_algorithm_club"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"rust_algorithm_club/collections/struct.SinglyLinkedList.html\" title=\"struct rust_algorithm_club::collections::SinglyLinkedList\">SinglyLinkedList</a>&lt;T&gt;","synthetic":false,"types":["rust_algorithm_club::collections::singly_linked_list::SinglyLinkedList"]}];
+implementors["rust_algorithm_club"] = [{"text":"impl&lt;T:&nbsp;Debug&gt; Debug for SinglyLinkedList&lt;T&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
